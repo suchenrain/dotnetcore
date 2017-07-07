430939c9ef8afd87d8fb3057d1c32cd3a4e3ce19
suchenxiaoyu <chen.zhuang@hpe.com>
2017-07-07 18:03:19 +0800

    last

---
+++ identity/ClientCredentials/IdentityServerWithEF/Config.cs
@@ -0,0 +1,139 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+
+namespace IdentityServerWithEF
+{
+    public class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1","My API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                    new Client
+                    {
+                        ClientId="client",
+                        AllowedGrantTypes=GrantTypes.ClientCredentials,
+                        ClientSecrets=
+                        {
+                            new Secret("secret".Sha256())
+                        },
+                        AllowedScopes={"api1"}
+                    },
+                    //owner pwd
+                    new Client
+                    {
+                        ClientId="ro.client",
+                        AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,
+                        ClientSecrets=
+                        {
+                            new Secret("secret".Sha256())
+                        },
+                        AllowedScopes={"api1"}
+                    },
+                    // OpenID Connect implicit flow client (MVC)
+                    new Client
+                    {
+                        ClientId = "mvc",
+                        ClientName = "MVC Client",
+                        AllowedGrantTypes = GrantTypes.Implicit,
+
+                        // where to redirect to after login
+                        RedirectUris = { "http://localhost:5002/signin-oidc" },
+
+                        // where to redirect to after logout
+                        PostLogoutRedirectUris = { "http://localhost:5002/signout-callback-oidc" },
+
+                        AllowedScopes = new List<string>
+                        {
+                            IdentityServerConstants.StandardScopes.OpenId,
+                            IdentityServerConstants.StandardScopes.Profile
+                        }
+                    },
+                    // OpenID Connect hybrid flow and client credentials client(MVC)
+                    new Client
+                    {
+                        ClientId="hybrid",
+                        ClientName="Hybrid Client",
+                        AllowedGrantTypes=GrantTypes.HybridAndClientCredentials,
+
+                        ClientSecrets=
+                        {
+                            new Secret("secret".Sha256())
+                        },
+
+                        // where to redirect to after login
+                        RedirectUris = { "http://localhost:5002/signin-oidc" },
+
+                        // where to redirect to after logout
+                        PostLogoutRedirectUris = { "http://localhost:5002/signout-callback-oidc" },
+
+                        AllowedScopes =
+                        {
+                            IdentityServerConstants.StandardScopes.OpenId,
+                            IdentityServerConstants.StandardScopes.Profile,
+                            "api1"
+                        },
+                        AllowOfflineAccess=true
+                    },
+                    // JavaScript Client
+                    new Client
+                    {
+                        ClientId = "js",
+                        ClientName = "JavaScript Client",
+                        AllowedGrantTypes = GrantTypes.Implicit,
+                        AllowAccessTokensViaBrowser = true,
+
+                        RedirectUris =           { "http://localhost:5003/callback.html" },
+                        PostLogoutRedirectUris = { "http://localhost:5003/index.html" },
+                        AllowedCorsOrigins =     { "http://localhost:5003" },
+
+                        AllowedScopes =
+                        {
+                            IdentityServerConstants.StandardScopes.OpenId,
+                            IdentityServerConstants.StandardScopes.Profile,
+                            "api1"
+                        }
+                    }
+            };
+
+        }
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId="1",
+                    Username="alice",
+                    Password="password"
+                },
+                new TestUser
+                {
+                    SubjectId="2",
+                    Username="bob",
+                    Password="password"
+                }
+            };
+        }
+    }
+}
---
+++ identity/ClientCredentials/IdentityServerWithEF/Startup.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using IdentityServer4.EntityFramework.Mappers;
+using IdentityServer4.EntityFramework.DbContexts;
+using System.Linq;
+
+namespace IdentityServerWithEF
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            var connectionString = @"server=(localdb)\mssqllocaldb;database=IdentityServer4.QuickStart.EntityFramework;trusted_connection=yes";
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            // configure identity server with in-memory users, but EF stores for clients and scopes
+            services.AddIdentityServer()
+                .AddTemporarySigningCredential()
+                .AddTestUsers(Config.GetUsers())
+                .AddConfigurationStore(builder =>
+                    builder.UseSqlServer(connectionString, options =>
+                        options.MigrationsAssembly(migrationsAssembly)))
+                .AddOperationalStore(builder =>
+                    builder.UseSqlServer(connectionString, options =>
+                        options.MigrationsAssembly(migrationsAssembly)));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            // this will do the initial DB population
+            InitializeDatabase(app);
+
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                scope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = scope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.GetClients())
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.GetIdentityResources())
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Config.GetApiResources())
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
+
+        }
+    }
+}
