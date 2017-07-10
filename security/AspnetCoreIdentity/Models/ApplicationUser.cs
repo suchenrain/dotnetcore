8ee8d2c2e0c58d86684e6ebb548401ac26cfc6b1
suchenxiaoyu <suchenxiaoyu@gmail.com>
2017-07-10 23:47:42 +0800

    config sqlserver

---
+++ security/AspnetCoreIdentity/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace AspnetCoreIdentity.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string SkypeAccount { get; set; }
+        public string LinkedinProfileLink { get; set; }
+        public string TwitterHandler { get; set; }
+    }
+}
